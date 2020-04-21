fa4b19c259289dcb69b26037465c2ebca0ae8d49
Jan Terue Ferran <janteruel@MacBook-MacBook-Pro-de-Jan.local>
2020-04-21 11:20:25 +0200

    Estadístiques i gràfics

--- src/app/participant-profile/participant-profile.component.ts
+++ src/app/participant-profile/participant-profile.component.ts
@@ -4,6 +4,7 @@ import { DataService } from 'app/services/data.service';
 import { ActivatedRoute } from '@angular/router';
 import { GlobalFunctionsService } from 'app/services/global-functions.service';
 import { GlobalStateService } from 'app/services/global-state.service';
+import * as Chartist from 'chartist';
 
 @Component({
   selector: 'app-participant-profile',
@@ -24,6 +25,7 @@ export class ParticipantProfileComponent implements OnInit {
       this.codi_participant = params.get("codi");
       this._ds.getParticipantByCodeWithFreeThrows(this.codi_participant).then(res => {
         this.participant = res;
+        this.generateCharts();
       })
       .catch(error => console.log(error));
     });
@@ -62,6 +64,68 @@ export class ParticipantProfileComponent implements OnInit {
       percentages[i] = parseInt(this._gfs.decimalRound(percentages[i] / this.participant.seriesTLL.length * 100));
     }
     return percentages;
+  }
+
+  private generateCharts() {
+    const dataFreeThrowsChart: any = {
+      labels: [],
+      series: []
+    };
+    let freeThrowsMade: number[] = [];
+    let jornada: number = 1;
+    while (jornada <= this.participant.seriesTLL.length) {
+      dataFreeThrowsChart['labels'].push(jornada);
+      freeThrowsMade.push(this.participant.seriesTLL[jornada - 1].anotats);
+      ++jornada;
+    }
+    dataFreeThrowsChart['series'].push(freeThrowsMade);
+
+    const optionsFreeThrowssChart: any = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+          tension: 0
+      }),
+      low: 0,
+      high: 4,
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+    }
+
+    var freeThrowsChart = new Chartist.Line('#freeThrowsChart', dataFreeThrowsChart, optionsFreeThrowssChart);
+
+    this.startAnimationForLineChart(freeThrowsChart);
+  }
+
+  private startAnimationForLineChart(chart) {
+    let seq: any, delays: any, durations: any;
+    seq = 0;
+    delays = 80;
+    durations = 500;
+
+    chart.on('draw', function(data) {
+      if(data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      } 
+      else if(data.type === 'point') {
+        seq++;
+        data.element.animate({
+          opacity: {
+            begin: seq * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
+    seq = 0;
   }
 
 }
